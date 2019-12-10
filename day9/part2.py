5bc1b067f423008db2951d6654feeb125dfcdb98
Ken Leidal <kkleidal@gmail.com>
2019-12-09 22:22:34 -0500

    cleaned up day9 scripts

--- day9/part2.py
+++ day9/part2.py
@@ -3,4 +3,4 @@ import sys
 from intcode import IntCodeProgram
 
 day9_input = list(map(int, sys.stdin.read().strip().split(",")))
-print(list(IntCodeProgram(day9_input).process().run([1]))[0])
+print(list(IntCodeProgram(day9_input).process().run([2]))[0])
